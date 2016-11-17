f0d1c2a6e64fbed26d56a3e0a05044288ecec7af
Tejas  Nikumbh <tejasnikumbh@tejass-macbook-pro.local>
2016-11-17 10:35:20 +0530

    Initial Commit

---
+++ WoW Sports Test AppTests/WoW_Sports_Test_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  WoW_Sports_Test_AppTests.swift
+//  WoW Sports Test AppTests
+//
+//  Created by Tejas  Nikumbh on 17/11/16.
+//  Copyright © 2016 Tejas  Nikumbh. All rights reserved.
+//
+
+import XCTest
+@testable import WoW_Sports_Test_App
+
+class WoW_Sports_Test_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
